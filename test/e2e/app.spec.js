e2251448f1da2f296404695096ea588c52784dd4
Giancarlo <gfabrizi@gmail.com>
2016-09-28 15:37:07 +0200

    karma setup

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -5,9 +5,17 @@ describe("app", function() {
   });
 });
 
-describe("app", function() {
-  it("should say 'Hello world' on the page", function() {
+// describe("app", function() {
+//   it("should say 'Hello world' on the page", function() {
+//     browser.get('/');
+//     expect($$("p").first().getText()).toEqual("Hello world");
+//   });
+// });
+
+describe('Todos tracker', function() {
+  it('has a todo', function() {
     browser.get('/');
-    expect($$("p").first().getText()).toEqual("Hello world");
+    var todo = $('#todo');
+    expect(todo.getText()).toEqual('ToDo1');
   });
 });
