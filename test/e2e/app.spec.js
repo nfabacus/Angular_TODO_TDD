8f252fdc2d1eb0481e34a34e49f1cbf8a094a01c
noby <n.fujioka@abacusmaths.info>
2016-09-28 12:45:25 +0100

    sets up protractor

---
+++ test/e2e/app.spec.js
@@ -0,0 +1,13 @@
+describe("app", function() {
+  it("should get home page title", function() {
+    browser.get('/');
+    expect(browser.getTitle()).toEqual("Todos App");
+  });
+});
+
+describe("app", function() {
+  it("should say 'Hello world' on the page", function() {
+    browser.get('/');
+    expect($$("p").first().getText()).toEqual("Hello world");
+  });
+});
